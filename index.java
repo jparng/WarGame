e75edb31753c8dd85635bc3b3ac183f65fa3db9a
Jerry Parng <59073672+jparng@users.noreply.github.com>
2022-09-26 19:29:50 -0500

    Update index.java

--- index.java
+++ index.java
@@ -1,1 +1,13 @@
 
+package WarGamePack;
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+
+public class index {
+	public static void main(String args[]) {
+		new gui();
+		
+	}
+}
