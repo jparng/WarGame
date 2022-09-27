1bbe320ad05786b05cb7788fa48fe420f1f568b3
Jerry Parng <59073672+jparng@users.noreply.github.com>
2022-09-26 19:31:59 -0500

    Add files via upload

---
+++ WarGamePack/gui.java
@@ -0,0 +1,37 @@
+package WarGamePack;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.*;
+public class gui {
+	
+	public gui() {
+		JFrame frame = new JFrame();
+		
+		JPanel panel = new JPanel();
+		
+		JButton firstButton = new JButton("1P");
+		JButton secondButton = new JButton("2P");
+		JLabel label = new JLabel("Card Game of War!");
+		
+		
+		panel.setBorder(BorderFactory.createEmptyBorder(300,300,100,300));
+		
+		panel.add(label);
+		panel.add(firstButton);
+		panel.add(secondButton);
+		panel.setLayout(new GridLayout(0, 1));
+		
+
+		frame.setTitle("Card Game of War");
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500,500);
+		frame.pack();
+//		frame.getContentPane().add(firstButton); //Adds button to content pane of frame
+//		frame.getContentPane().add(secondButton);
+		frame.setVisible(true);
+		
+	}
+	
+}
---
+++ WarGamePack/index.java
@@ -0,0 +1,12 @@
+package WarGamePack;
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+
+public class index {
+	public static void main(String args[]) {
+		new gui();
+		
+	}
+}
