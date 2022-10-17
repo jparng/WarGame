93ee392d9a8037d579aaef46b63bc3b683b96d97
Jerry Parng <59073672+jparng@users.noreply.github.com>
2022-10-16 19:41:06 -0500

    add card back images to spoils deck

--- WarGamePack/gui.java
+++ WarGamePack/gui.java
@@ -3,10 +3,11 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.lang.Object.*;
-
+import javax.sound.sampled.*;
 
 
 public class gui {
+	
 	
 	public gui() {
 		JFrame frame = new JFrame();
@@ -16,34 +17,30 @@ public class gui {
 		//Initialize panel and panel layout
 		JPanel panel = new JPanel();
 		panel.setBounds(0, 0, 1280, 720);
-		
-		
-		//adds background image - Ryan M.
-
-		JButton secondButton = new JButton("2P");
-		secondButton.setFont(new Font("Arial", Font.PLAIN, 14));
-		secondButton.setBounds(641, 322, 165, 76);
 		JButton resetBut = new JButton("Reset");
 		resetBut.setVisible(false);
 		resetBut.setBounds(1151, 11, 91, 35);
 		panel.setLayout(null);
 		
+		JLabel mainDeck2 = new JLabel("");
+		mainDeck2.setIcon(new ImageIcon(gui.class.getResource("/resources/Cards/Card-Back-Resized.jpg")));
+		mainDeck2.setBounds(958, 219, 165, 239);
+		panel.add(mainDeck2);
+		mainDeck2.setVisible(false);
 		
-
+		JLabel mainDeck1 = new JLabel("");
+		mainDeck1.setIcon(new ImageIcon(gui.class.getResource("/resources/Cards/Card-Back-Resized.jpg")));
+		mainDeck1.setBounds(175, 219, 156, 230);
+		panel.add(mainDeck1);
+		mainDeck1.setVisible(false);
 		
-
-		
-		
-		JButton firstButton = new JButton("1P");
-		firstButton.setFont(new Font("Arial", Font.PLAIN, 14));
-		firstButton.setBounds(408, 322, 165, 76);
-		panel.add(firstButton);
-		
-		
-
-		
-
-		panel.add(secondButton);
+		JLabel title = new JLabel("Game of War!");
+		title.setForeground(new Color(0, 0, 0));
+		title.setBackground(new Color(255, 255, 255));
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+		title.setFont(new Font("Arial", Font.PLAIN, 50));
+		title.setBounds(472, 81, 316, 59);
+		panel.add(title);
 		panel.add(resetBut);
 
 		
@@ -57,35 +54,72 @@ public class gui {
 		JButton draw = new JButton("Draw");
 		draw.setBounds(533, 524, 177, 82);
 		draw.setVisible(false);
+		
+		
+
+		
+		
+		JButton firstButton = new JButton("1P");
+		firstButton.setFont(new Font("Arial", Font.PLAIN, 14));
+		firstButton.setBounds(408, 322, 165, 76);
+		panel.add(firstButton);
+		
+		
+		
+
+		
+		
+		//adds background image - Ryan M.
+
+		JButton secondButton = new JButton("2P");
+		secondButton.setFont(new Font("Arial", Font.PLAIN, 14));
+		secondButton.setBounds(641, 322, 165, 76);
+		
+		
+
+		
+
+		panel.add(secondButton);
+		
+
 		panel.add(draw);
 		
-		JLabel title = new JLabel("Game of War!");
-		title.setHorizontalAlignment(SwingConstants.CENTER);
-		title.setFont(new Font("Arial", Font.PLAIN, 50));
-		title.setBounds(472, 81, 316, 59);
-		panel.add(title);
-		
-		JLabel backGround1 = new JLabel("");
-		backGround1.setIcon(new ImageIcon("C:\\Users\\Jerry Parng\\eclipse-workspace\\WarGame\\bin\\resources\\bg.png"));
-		backGround1.setBounds(0, 1, 1270, 690);
-		panel.add(backGround1);
-		
 		JLabel backGround2 = new JLabel("");
-		backGround2.setIcon(new ImageIcon("C:\\Users\\Jerry Parng\\eclipse-workspace\\WarGame\\bin\\resources\\bg_1play.jpg"));
+		backGround2.setIcon(new ImageIcon(gui.class.getResource("/resources/bg_1play.jpg")));
 		backGround2.setBounds(0, 0, 1270, 691);
 		panel.add(backGround2);
 		
+		JLabel backGround1 = new JLabel("");
+		backGround1.setIcon(new ImageIcon(gui.class.getResource("/resources/bg.png")));
+		backGround1.setBounds(0, 1, 1270, 690);
+		panel.add(backGround1);
+		
 		JLabel backGround3 = new JLabel("");
-		backGround3.setIcon(new ImageIcon("C:\\Users\\Jerry Parng\\eclipse-workspace\\WarGame\\bin\\resources\\bg_2play.jpg"));
+		backGround3.setIcon(new ImageIcon(gui.class.getResource("/resources/bg_2play.jpg")));
 		backGround3.setBounds(0, 1, 1270, 690);
 		panel.add(backGround3);
+		
+		JLabel activeCard1 = new JLabel("");
+		activeCard1.setBounds(377, 183, 212, 307);
+		panel.add(activeCard1);
+		
+		JLabel activeCard2 = new JLabel("");
+		activeCard2.setBounds(693, 183, 212, 307);
+		panel.add(activeCard2);
+		
+		JLabel spoilsDeck1 = new JLabel("");
+		spoilsDeck1.setBounds(50, 265, 103, 154);
+		panel.add(spoilsDeck1);
+		
+		JLabel spoilsDeck2 = new JLabel("");
+		spoilsDeck2.setBounds(1126, 265, 103, 154);
+		panel.add(spoilsDeck2);
 	
 		backGround2.setVisible(false);
 		backGround3.setVisible(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		frame.setVisible(true);
-		
 		
 		
 		//if 1p or 2p is clicked, hide buttons and create second layout with decks, draw buttons, win counter, music toggle, and speed toggle
@@ -98,8 +132,12 @@ public class gui {
 				resetBut.setVisible(true);
 				backGround1.setVisible(false);
 				backGround2.setVisible(true);
+				mainDeck1.setVisible(true);
+				mainDeck2.setVisible(true);
+				
 			}
 		});
+		
 		
 		//2 player button
 		secondButton.addActionListener(new ActionListener() {
@@ -112,8 +150,13 @@ public class gui {
 				backGround1.setVisible(false);
 				backGround2.setVisible(false);
 				backGround3.setVisible(true);
+				mainDeck1.setVisible(true);
+				mainDeck2.setVisible(true);
 			}
 		});
+		
+		
+		
 		
 		//Resets all buttons and title 
 		resetBut.addActionListener(new ActionListener() {
@@ -126,11 +169,21 @@ public class gui {
 				backGround1.setVisible(true);
 				backGround2.setVisible(false);
 				backGround3.setVisible(false);
+				mainDeck1.setVisible(false);
+				mainDeck2.setVisible(false);
+			}
+		});
+		
+		draw.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//draws the cards
+				index.drawCard();
+
 			}
 		});
 	
+
+		
+		
 	}
-
 }
-
-
