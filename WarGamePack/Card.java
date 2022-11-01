0c72d3222e7e62fe23ee7bab576aa88154c74321
Jerry Parng <59073672+jparng@users.noreply.github.com>
2022-10-31 20:44:02 -0500

    Removed old deck initialization

--- WarGamePack/Card.java
+++ WarGamePack/Card.java
@@ -72,31 +72,7 @@ public class Card {
 	        return rank == c.rank && suit == c.suit;
 	    }
 	
-	    public static String[] newDeck() {
-			int n = suits.length * ranks.length;
-			
-			String[] deck = new String[n];
-	    	for(int i = 0; i < ranks.length; i++) {
-	    		for(int j = 0; j < suits.length; j++) {
-	    			deck[suits.length * i + j] = ranks[i] + " of " + suits[j];
-	    		}
-	    	}
-	    	
-	    	//Shuffles the deck
-	    	for(int i = 0; i < n; i++) {
-	    		int r = i + (int)(Math.random() * (n - i));
-	    		String temp = deck[r];
-	    		deck[r] = deck[i];
-	    		deck[i] = temp;
-	    	}
-			
-			
-	    	return deck;
-	    	
-	    }
-	
-	
-	
+
 	    
 	    public ImageIcon getImage() throws IOException {
 	    	if(cardImage == null) {
