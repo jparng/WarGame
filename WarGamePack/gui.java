c9935fc116d7caf378d9c739265a20db1f9d52ff
Jerry Parng <59073672+jparng@users.noreply.github.com>
2022-10-31 22:52:19 -0500

    Added card labels to Active cards and spoils decks

--- WarGamePack/gui.java
+++ WarGamePack/gui.java
@@ -8,11 +8,13 @@ import javax.sound.sampled.*;
 
 public class gui {
 	
-	
 	public gui() {
+		
+		
 		JFrame frame = new JFrame();
 		frame.setResizable(false);
 		frame.setBounds(0, 0, 1280, 720);
+		
 		
 		//Initialize panel and panel layout
 		JPanel panel = new JPanel();
@@ -22,14 +24,26 @@ public class gui {
 		resetBut.setBounds(1151, 11, 91, 35);
 		panel.setLayout(null);
 		
+		JLabel spoilsDeck2 = new JLabel("");
+		spoilsDeck2.setIcon(new ImageIcon(gui.class.getResource("/Cards/Card-Back-Spoils.jpg")));
+		spoilsDeck2.setBounds(1121, 242, 121, 189);
+		panel.add(spoilsDeck2);
+		spoilsDeck2.setVisible(false);
+		
+		JLabel spoilsDeck1 = new JLabel("");
+		spoilsDeck1.setIcon(new ImageIcon(gui.class.getResource("/Cards/Card-Back-Spoils.jpg")));
+		spoilsDeck1.setBounds(42, 242, 123, 189);
+		panel.add(spoilsDeck1);
+		spoilsDeck1.setVisible(false);
+		
 		JLabel mainDeck2 = new JLabel("");
-		mainDeck2.setIcon(new ImageIcon(gui.class.getResource("/resources/Cards/Card-Back-Resized.jpg")));
+		mainDeck2.setIcon(new ImageIcon(gui.class.getResource("/Cards/Card-Back-Resized.jpg")));
 		mainDeck2.setBounds(958, 219, 165, 239);
 		panel.add(mainDeck2);
 		mainDeck2.setVisible(false);
 		
 		JLabel mainDeck1 = new JLabel("");
-		mainDeck1.setIcon(new ImageIcon(gui.class.getResource("/resources/Cards/Card-Back-Resized.jpg")));
+		mainDeck1.setIcon(new ImageIcon(gui.class.getResource("/Cards/Card-Back-Resized.jpg")));
 		mainDeck1.setBounds(175, 219, 156, 230);
 		panel.add(mainDeck1);
 		mainDeck1.setVisible(false);
@@ -39,7 +53,7 @@ public class gui {
 		title.setBackground(new Color(255, 255, 255));
 		title.setHorizontalAlignment(SwingConstants.CENTER);
 		title.setFont(new Font("Arial", Font.PLAIN, 50));
-		title.setBounds(472, 81, 316, 59);
+		title.setBounds(448, 82, 316, 59);
 		panel.add(title);
 		panel.add(resetBut);
 
@@ -77,47 +91,40 @@ public class gui {
 		
 		
 
-		
-
 		panel.add(secondButton);
-		
-
 		panel.add(draw);
 		
+		JLabel activeCard1 = new JLabel("");
+		activeCard1.setIcon(new ImageIcon(gui.class.getResource("/Cards/Card-Back-Active.jpg")));
+		activeCard1.setBounds(370, 169, 231, 329);
+		panel.add(activeCard1);
+		activeCard1.setVisible(false);
+		
+		JLabel activeCard2 = new JLabel("");
+		activeCard2.setIcon(new ImageIcon(gui.class.getResource("/Cards/Card-Back-Active.jpg")));
+		activeCard2.setBounds(680, 169, 231, 329);
+		panel.add(activeCard2);
+		activeCard2.setVisible(false);
+		
 		JLabel backGround2 = new JLabel("");
-		backGround2.setIcon(new ImageIcon(gui.class.getResource("/resources/bg_1play.jpg")));
+		backGround2.setIcon(new ImageIcon(gui.class.getResource("/Backgrounds/bg_1play.jpg")));
 		backGround2.setBounds(0, 0, 1270, 691);
 		panel.add(backGround2);
 		
 		JLabel backGround1 = new JLabel("");
-		backGround1.setIcon(new ImageIcon(gui.class.getResource("/resources/bg.png")));
+		backGround1.setIcon(new ImageIcon(gui.class.getResource("/Backgrounds/bg.png")));
 		backGround1.setBounds(0, 1, 1270, 690);
 		panel.add(backGround1);
 		
 		JLabel backGround3 = new JLabel("");
-		backGround3.setIcon(new ImageIcon(gui.class.getResource("/resources/bg_2play.jpg")));
+		backGround3.setIcon(new ImageIcon(gui.class.getResource("/Backgrounds/bg_2play.jpg")));
 		backGround3.setBounds(0, 1, 1270, 690);
 		panel.add(backGround3);
-		
-		JLabel activeCard1 = new JLabel("");
-		activeCard1.setBounds(377, 183, 212, 307);
-		panel.add(activeCard1);
-		
-		JLabel activeCard2 = new JLabel("");
-		activeCard2.setBounds(693, 183, 212, 307);
-		panel.add(activeCard2);
-		
-		JLabel spoilsDeck1 = new JLabel("");
-		spoilsDeck1.setBounds(50, 265, 103, 154);
-		panel.add(spoilsDeck1);
-		
-		JLabel spoilsDeck2 = new JLabel("");
-		spoilsDeck2.setBounds(1126, 265, 103, 154);
-		panel.add(spoilsDeck2);
 	
 		backGround2.setVisible(false);
 		backGround3.setVisible(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 
 		frame.setVisible(true);
 		
@@ -135,8 +142,8 @@ public class gui {
 				mainDeck1.setVisible(true);
 				mainDeck2.setVisible(true);
 				
-				//Initializes deck
 				index.startGame();
+				
 			}
 		});
 		
@@ -155,7 +162,6 @@ public class gui {
 				mainDeck1.setVisible(true);
 				mainDeck2.setVisible(true);
 				
-				//Initializes deck
 				index.startGame();
 			}
 		});
@@ -176,6 +182,11 @@ public class gui {
 				backGround3.setVisible(false);
 				mainDeck1.setVisible(false);
 				mainDeck2.setVisible(false);
+				spoilsDeck1.setVisible(false);
+				spoilsDeck2.setVisible(false);
+				activeCard1.setVisible(false);
+				activeCard2.setVisible(false);
+				
 				
 				index.Reset();
 			}
@@ -185,12 +196,24 @@ public class gui {
 			public void actionPerformed(ActionEvent e) {
 				//draws the cards
 				index.drawCard();
+				
+				spoilsDeck1.setVisible(true);
+				spoilsDeck2.setVisible(true);
+				activeCard1.setVisible(true);
+				activeCard2.setVisible(true);
 
 			}
 		});
+		
+		
+			
+	}
 	
 
-		
+
+	public static void updateAll() {
+		// TODO Auto-generated method stub
 		
 	}
 }
+
