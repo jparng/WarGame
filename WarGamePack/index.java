65de78bd2b9e050564e7788942127cff8faf0e3b
Jerry Parng <59073672+jparng@users.noreply.github.com>
2022-10-31 20:50:01 -0500

    Added Linkedlist deck initialization

--- WarGamePack/index.java
+++ WarGamePack/index.java
@@ -67,6 +67,44 @@ public class index extends javax.swing.JFrame {
 		gui = new gui();
 	}
 	
+	
+	//Splits deck into 26 cards for Player 1
+	public static LinkedList<Card> firstDeck(LinkedList<Card> deck1, LinkedList<Card> fullDeck){
+		//takes 1st half of deck and adds to the Player 1's deck
+		for(int i = 0; i < fullDeck.size() / 2; i++) {
+			deck1.add(fullDeck.get(i));
+		}
+		return deck1;
+	}
+	//Splits deck into 26 cards for Player 2
+	public static LinkedList<Card> secondDeck(LinkedList<Card> deck2, LinkedList<Card> fullDeck) {
+		//takes  2nd half of deck and adds to Player 2's deck
+		for(int i = fullDeck.size() - 1; i >= fullDeck.size() / 2; --i) {
+			deck2.add(fullDeck.get(i));
+		}
+		return deck2;
+	}
+	
+	//Initializes the full deck
+    	public static LinkedList<Card> newDeck(LinkedList<Card> deck) throws IOException {
+        // 4 suits
+        for(int i = 0; i < 4; i++) {
+        	//13 ranks
+        	for(int j = 2; j <= 14; j++) {
+        		deck.add(new Card(j, i));
+        	}
+        }
+        
+        Collections.shuffle(deck);
+		return deck;
+    }
+	
+	
+	
+	
+	
+	
+	
 	public static void startGame() {
 		
 	try {
