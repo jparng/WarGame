1483843aa7f3e34dc597542f1ed7079c044a2bc5
Jerry Parng <59073672+jparng@users.noreply.github.com>
2022-11-15 20:08:02 -0600

    Updated Sound effects
    Sound effects should work when the sound files are together now.

--- WarGamePack/Sound.java
+++ WarGamePack/Sound.java
@@ -4,34 +4,36 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.InputStream;
+import java.net.URL;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
+@SuppressWarnings("unused")
 public class Sound {
 	
-	String winHand, winGame, shuffle, drawCard;
-	SoundEffect se = new SoundEffect();
+	URL winGame;
+	URL winHand;
+	URL shuffle;
+	URL drawCard;
+	URL loseHand;
+	Clip clip;
 	
 	public Sound() {
 		
-		winHand = ".//Sound_Effects//winHand.wav";
-		winGame = ".//Sound_Effects//GameWon.wav";
-		shuffle = ".//Sound_Effects//shuffle.wav";
-		drawCard = ".//Sound_Effects//card.wav";
+		winHand = getClass().getResource("/Sound_Effects/winHand.wav");
+		winGame = getClass().getResource("/Sound_Effects/GameWon.wav");
+		shuffle = getClass().getResource("/Sound_Effects/shuffle.wav");
+		drawCard = getClass().getResource("/Sound_Effects/card.wav");
+		loseHand = getClass().getResource("/Sound_Effects/loseHand.wav");
 	}
 	
-public class SoundEffect {
-		
-		Clip clip;
-		
-		
-		public void setFile(String soundFileName) {
+
+	public void setFile(URL s) {
 			
 			try {
-				File file = new File(soundFileName);
-				AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+				AudioInputStream sound = AudioSystem.getAudioInputStream(s);
 				clip = AudioSystem.getClip();
 				clip.open(sound);
 			}
@@ -45,16 +47,24 @@ public class SoundEffect {
 			clip.setFramePosition(0);
 			clip.start();
 		}
-	}
-	
-	//public class ButtonHandler implements ActionListener {
-	//	public void actionPerformed(ActionEvent event) {
-	//		se.setFile(shuffle);
-	//		se.play();
-	//	}
-	//}
-	
+		
+		public void shuffle(){
+			setFile(shuffle);
+			play();
+		}
+		public void draw() {
+			setFile(drawCard);
+			play();
+		}
+		
+		public void winHand() {
+			setFile(winHand);
+			play();
+		}
+		public void loseHand() {
+			setFile(loseHand);
+			play();
+		}
 
 }
-	
 	
